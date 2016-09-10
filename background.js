c635bc7fe9480538eb60a6377a316cef17c9d1b9
bradley inniss <bfinniss@gmail.com>
2016-09-10 02:34:03 -0700

    added jQuery

--- background.js
+++ background.js
@@ -1,11 +1,4 @@
-
-// console.log("working")
-
-document.getElementsByClassName("lolomoRow lolomoRow_title_card originals-panels-row lolomo-row-above-nav")[0].style.display = "none";
-console.log(document.readyState)
-
-window.onload = function() {
-document.getElementsByClassName("lolomoRow lolomoRow_title_card originals-panels-row lolomo-row-above-nav")[0].style.display = "none";
-console.log(document.readyState)
-
-}
+var myFunction = function() {
+    document.getElementsByClassName("lolomoRow lolomoRow_title_card originals-panels-row lolomo-row-above-nav")[0].style.display = "none";
+};
+setInterval(myFunction, 1000);
