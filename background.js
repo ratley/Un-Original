46fc17547beeaaec359d49723462355bbf0221fa
bradley inniss <bfinniss@gmail.com>
2016-09-10 00:54:14 -0700

    initial commit

---
+++ background.js
@@ -0,0 +1,11 @@
+
+// console.log("working")
+
+document.getElementsByClassName("lolomoRow lolomoRow_title_card originals-panels-row lolomo-row-above-nav")[0].style.display = "none";
+console.log(document.readyState)
+
+window.onload = function() {
+document.getElementsByClassName("lolomoRow lolomoRow_title_card originals-panels-row lolomo-row-above-nav")[0].style.display = "none";
+console.log(document.readyState)
+
+}
